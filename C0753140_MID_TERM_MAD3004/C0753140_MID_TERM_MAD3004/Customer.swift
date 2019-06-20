0f802b7d96fc3796ae08fb3c10874a68ce8aeed7
Gurwinder SIngh <gurwinder958652@gmail.com>
2019-06-20 19:07:15 -0400

    [Gurwinder ] Customer class created

---
+++ C0753140_MID_TERM_MAD3004/C0753140_MID_TERM_MAD3004/Customer.swift
@@ -0,0 +1,13 @@
+//
+//  Customer.swift
+//  C0753140_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-06-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+class Customer{
+    
+}
--- C0753140_MID_TERM_MAD3004/C0753140_MID_TERM_MAD3004/Mobile.swift
+++ C0753140_MID_TERM_MAD3004/C0753140_MID_TERM_MAD3004/Mobile.swift
@@ -11,4 +11,14 @@ class Mobile: Bill {
     // declaring mobile class variables
     var mobileManufacturerName : String
     var planName : String
+    
+    //Initializing the Mobile class variables
+    
+    init(id_bill: Int, date_of_bill: Date, total_bill_amount: Float,mobileManufacturerName: String,planName: String) {
+        
+        self.mobileManufacturerName = mobileManufacturerName
+        self.planName = planName
+        super.init(id_bill: <#T##Int#>, date_of_bill: <#T##Date#>, total_bill_amount: <#T##Float#>)
+    }
+
 }
