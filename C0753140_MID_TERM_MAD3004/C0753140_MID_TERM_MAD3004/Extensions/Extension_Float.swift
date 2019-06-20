84ac010db5f5d1de75f4e0fee653bf8217014bba
Gurwinder SIngh <gurwinder958652@gmail.com>
2019-06-20 19:17:02 -0400

    [ Gurwinder ] Extenson created for currency

---
+++ C0753140_MID_TERM_MAD3004/C0753140_MID_TERM_MAD3004/Extensions/Extension_Float.swift
@@ -0,0 +1,17 @@
+//
+//  Extension_Float.swift
+//  C0753140_MID_TERM_MAD3004
+//
+//  Created by MacStudent on 2019-06-20.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import Foundation
+
+extension Float
+{
+    public func currency() -> String
+    {
+        return String.init(format: "$%0.2f", self)
+    }
+}
